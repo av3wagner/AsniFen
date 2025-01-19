f09ed6355659558f2e9760cf4c4b3a39a0980a4c
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-01-19 06:41:03 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,3 +1,31 @@
 import pandas as pd
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import sqlite3
+import dash
+from dash import dash_table
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+from dash import Dash, dcc, html, Input, Output, State, callback
+import plotly.express as px
+import plotly.graph_objects as go
+import chart_studio.plotly as py 
+from jupyter_dash import JupyterDash
+import flask
+import json
+import requests
+from urllib.request import urlopen
+from prophet import Prophet
+from pandas_datareader import data, wb
+import base64
+
 raw_df = pd.read_csv('data/heart.csv')
+app = JupyterDash(external_stylesheets=[dbc.themes.SLATE])
+server=app.server
 print(raw_df.head(10))
+
+if __name__ == '__main__':
+    app.run(debug=False)
