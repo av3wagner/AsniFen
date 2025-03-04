caad3acee5b9d03c715ec6ad6527764c58f8f7a7
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-04 13:14:51 +0100

    Update ReadPath.py

--- modules/ReadPath.py
+++ modules/ReadPath.py
@@ -19,7 +19,7 @@ def execute_python_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             python_code = file.read()
-            exec(python_code)
+        exec(python_code)
     except FileNotFoundError:
         st.markdown(f"Error: The file '{file_path}' does not exist.")
 
