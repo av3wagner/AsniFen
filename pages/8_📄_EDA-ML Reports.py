08c22a35f669238c0a92fcc96feb6c7d59cacec2
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-07 17:10:01 +0100

    Update 8_📄_EDA-ML Reports.py

--- pages/8_📄_EDA-ML Reports.py
+++ pages/8_📄_EDA-ML Reports.py
@@ -31,8 +31,7 @@ st.set_page_config(
 
 #m.select_file()  
 #"/mount/src/asnifen/modules/ReadPath.py"
-cwd = os.getcwd()
-Reports=os.path.join(cwd, "modules/programs/EDA-Reports.py")
+#cwd = os.getcwd()
+#Reports=os.path.join(cwd, "modules/programs/EDA-Reports.py")
 #print(EDA-Reports)
-exec(open(Reports).read(), globals())
-
+exec(open("/mount/src/asnifen/modules/programs/EDA-Reports.py").read(), globals())
