6a1a2b80e154cedec0359127ea83abc01e4cdaa9
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-09 11:55:53 +0100

    Update 8_📄_EDA-ML Reports.py

--- pages/8_📄_EDA-ML Reports.py
+++ pages/8_📄_EDA-ML Reports.py
@@ -22,7 +22,7 @@ import seaborn as sns
 os.chdir("/mount/src/asnifen/")
 sys.path.append("/mount/src/asnifen/")
 sys.path.append("/mount/src/asnifen/modules")
-import custom_module
+import modules
 from . import ReadPath
 
 st.set_page_config(
