4d16a0db168a3dbb4c364c34ace3417d4787ebba
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-11 07:15:13 +0100

    Update Reports.py

--- modules/programs/Reports.py
+++ modules/programs/Reports.py
@@ -11,11 +11,11 @@ sys.path.append("/mount/src/asnifen/assets")
 sys.path.append("/mount/src/asnifen/modules")
 sys.path.append("/mount/src/asnifen/modules/programs")
 
-with open("/mount/src/asnifen/assets/EDA01.jpg ", "rb") as img_file:    
-        img01 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+with open("EDA01.jpg ", "rb") as img_file:    
+    img01 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
         
-with open("/mount/src/asnifen/assets/EDA02.jpg ", "rb") as img_file:    
-        img02 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()   
+with open("EDA02.jpg ", "rb") as img_file:    
+    img02 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()   
         
 columns=['age', 'operation_year', 'lymph_nodes', 'survival_status']
 data=pd.read_csv("/mount/src/asnifen/data/haberman.csv",names=columns)
