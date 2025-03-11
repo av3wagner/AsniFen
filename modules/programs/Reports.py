84653325123b41a551eb10f5d10aa26c1330e2ab
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-11 07:41:09 +0100

    Update Reports.py

--- modules/programs/Reports.py
+++ modules/programs/Reports.py
@@ -1,9 +1,24 @@
+#import streamlit as st  
+#import os, sys
+#from os import listdir
+#from os.path import isfile, join
+#import pathlib
+#import base64
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
 import streamlit as st  
-import os, sys
+import streamlit.components.v1 as components
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import base64
+import sys
+import inspect, os
+import pathlib
 from os import listdir
 from os.path import isfile, join
-import pathlib
-import base64
+import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
 
