72f35448c35fc60764133442980999325325d3d9
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-11 07:06:14 +0100

    Update ImportBib.py

--- modules/ImportBib.py
+++ modules/ImportBib.py
@@ -5,36 +5,12 @@ from  PIL import Image
 import numpy as np
 import pandas as pd
 import base64
-import sys
-import inspect, os
+import os, sys
+import inspect
 import pathlib
 from os import listdir
 from os.path import isfile, join
 import glob
 import gunicorn
-
-#ReadPath
-import streamlit as st  
-import os, sys
-from os import listdir
-from os.path import isfile, join
-import pathlib
-import base64
-
-#Reports
-import streamlit as st  
-import streamlit.components.v1 as components
-from  PIL import Image
-import numpy as np
-import pandas as pd
-import base64
-import sys
-import inspect, os
-import pathlib
-from os import listdir
-from os.path import isfile, join
-import glob
-import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-warnings.filterwarnings("ignore")
