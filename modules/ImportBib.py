74163c65c54fb563df1e838ff31a46c707e88498
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-11 06:38:22 +0100

    Update ImportBib.py

--- modules/ImportBib.py
+++ modules/ImportBib.py
@@ -1,3 +1,19 @@
+#main
+import streamlit as st  
+import streamlit.components.v1 as components
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import base64
+import sys
+import inspect, os
+import pathlib
+from os import listdir
+from os.path import isfile, join
+import glob
+import gunicorn
+
+
 from IPython.core.display import HTML
 from PIL import Image
 from collections import Counter
