ff43d76c7b336c90dd01b6001a21a4bd0a6efeb5
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-12 06:02:10 +0100

    Update ReadPath.py

--- modules/ReadPath.py
+++ modules/ReadPath.py
@@ -24,6 +24,14 @@ def execute_python_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             python_code = file.read()
+            exec(python_code)
+    except FileNotFoundError:
+        st.markdown(f"Error: The file '{file_path}' does not exist.")
+        
+def execute_python_file_New(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            python_code = file.read()
         exec(python_code, globals())
         #EDA-Reports=os.path.join(cwd, "modules/programs/EDA-Reports.py")
         #print(EDA-Reports)
