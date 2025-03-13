3c31f184dad7a11ac6cca16f89fd65a9bc84430c
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-13 12:56:05 +0100

    Update MLKagle.py

--- modules/programs/MLKagle.py
+++ modules/programs/MLKagle.py
@@ -14,20 +14,12 @@ from sklearn.utils.multiclass import unique_labels
 os.chdir("/mount/src/asnifen/")
 sys.path.append("/mount/src/asnifen/")
 
-def main():
+def Rmain():
     st.title("Heart Disease Classification")
-    #st.sidebar.title("Heart Disease Classification")
-    #st.markdown("Is patient has Heart disease")
-    #st.sidebar.markdown("Is patient has Heart disease")
 
     @st.cache(persist=True)
     def load_data():
         data = pd.read_csv('/mount/src/asnifen/data/heart.csv')
-        """
-        labelencoder = LabelEncoder()
-        for col in data.columns:
-            data[col] = labelencoder.fit(data[col])
-        """
         return data
 
 
@@ -43,48 +35,37 @@ def main():
         return x_train, x_test, y_train, y_test
 
     def plot_metrics(metrics_list):
-        if 'Confusion Matrix' in metrics_list:
+        if metrics_list == 'Confusion Matrix':
             st.subheader('Confusion Matrix')
             plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
             st.pyplot()
 
-        if 'ROC Curve' in metrics_list:
+        if metrics_list == 'ROC Curve':
             st.subheader('ROC Curve')
             plot_roc_curve(model, x_test, y_test)
             st.pyplot()
 
-        if 'Precision-Recall Curve' in metrics_list:
+        if metrics_list == 'Precision-Recall Curve':
             st.subheader('Precision-Recall Curve')
             plot_precision_recall_curve(model, x_test, y_test)
             st.pyplot()
 
-    df = load_data()
-    class_names = ['no', 'yes']
-    #class_names = unique_labels(df.target)
+df = load_data()
+class_names = ['no', 'yes']
+x_train, x_test, y_train, y_test = split(df)
 
-    x_train, x_test, y_train, y_test = split(df)
-    st.sidebar.subheader("Choose Classifier")
-    #classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
-
-    classifier = "Support Vector Machine (SVM)"
-    if classifier == 'Support Vector Machine (SVM)':
-        #st.sidebar.subheader('Model Hyperparameters')
-        C = 5.0            #st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
-        kernel = "sigmoid" #st.sidebar.radio("Kernel", ("rbf", "linear", "poly", "sigmoid"), key='kernel')
-        gamma = "auto"     #st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
-
-        #metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-        #if st.sidebar.button("Classify", key='classify'):
-            #st.subheader("Support Vector Machine (SVM) Results")
-        
-            model = SVC(C=C, kernel=kernel, gamma=gamma)
-            model.fit(x_train, y_train)
-            accuracy = model.score(x_test, y_test)
-            y_pred = model.predict(x_test)
-            st.write("Accuracy: ", accuracy.round(2))
-            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
-            #plot_metrics(metrics)
+classifier = "Support Vector Machine (SVM)"
+if classifier == 'Support Vector Machine (SVM)':
+        C = 5.0            
+        kernel = "sigmoid" 
+        gamma = "auto"     
+        model = SVC(C=C, kernel=kernel, gamma=gamma)
+        model.fit(x_train, y_train)
+        accuracy = model.score(x_test, y_test)
+        y_pred = model.predict(x_test)
+        st.write("Accuracy: ", accuracy.round(2))
+        st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+        st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
         
          st.write('Confusion Matrix')
          plot_metrics('Confusion Matrix')
@@ -94,6 +75,3 @@ def main():
         
          st.write('Precision-Recall Curve')             
          plot_metrics('Precision-Recall Curve')
-
-if __name__ == '__main__':
-    main()
