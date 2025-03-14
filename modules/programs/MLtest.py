ff2fc31c78abbe65ed8b617f07459374817d454a
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-14 06:19:33 +0100

    Update MLtest.py

--- modules/programs/MLtest.py
+++ modules/programs/MLtest.py
@@ -184,14 +184,17 @@ if classifier == 'Support Vector Machine (SVM)':
     col1, col2, col3,= st.columns([1, 7, 1])
     with col2:
         st.pyplot() 
-
-classifier = "Support Vector Machine (SVM)"
-if classifier == 'Support Vector Machine (SVM)':
-    C = 5.0            
+        
+classifier = "Logistic Regression"
+if classifier == 'Logistic Regression':
+    C = 5.0           
+    max_iter = 200 
     kernel = "sigmoid" 
     gamma = "auto"  
     clf = SVC(random_state=0)
     model = SVC(C=C, kernel=kernel, gamma=gamma)
+
+
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
     y_pred = model.predict(X_test)
