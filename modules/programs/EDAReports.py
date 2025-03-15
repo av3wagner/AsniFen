68f9753da39c78b2c95409ed4dcc56e6f1411fc6
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-15 06:41:45 +0100

    Update EDAReports.py

--- modules/programs/EDAReports.py
+++ modules/programs/EDAReports.py
@@ -13,6 +13,7 @@ from os.path import isfile, join
 import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 with open("/mount/src/asnifen/assets/EDA01.jpg", "rb") as img_file:    
         img01 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
         
